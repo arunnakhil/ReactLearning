ce1f3a857547ca163c541967921f8ceecd12a819
arunnakhil <89119363+arunnakhil@users.noreply.github.com>
2021-08-18 10:22:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,59 @@
+
+// /*
+//   let user = ["Dhoni","Raina","Jadeja",7,5];
+
+//   console.log(user.shift());
+
+//   user.unshift("Rayudu");
+
+
+//   user.push("KL Rahul");
+//   user.push(2);
+
+//   user.pop(); 
+
+//   console.log(user.length);
+
+//   console.log(user);*/
+
+// //   let items = [
+// //       ["captain","VC","Bowler"],
+// //       ["Dhoni","Raina","Jadeja"],
+// //       [07,03,08]
+// //   ];
+
+// // //   delete items[1][2];
+// // //   delete items[2][1];
+
+// //   items.splice(1,1,["kohli","ABD",2,4,8,21]);
+
+// //   console.log(items);
+
+
+// // let color =["green","black","yellow"];
+// // console.log(color[2]);
+// // console.log(shift)
+
+
+//  result=0;
+//  let code=[10,20,30,40,50,60,70,80,90,100];
+//  for(i=0;i<code.length;i++){
+//    console.log(result+=code[i])
+//    i=i+1;
+//    console.log(result+=code[i+1])
+//    i=i+1;
+
+//   // }
+//   // let boat=[console.log(code.slice(7,10))];
+//   // console.log(code[0]);
+// //  console.log(boat);
+// //  console.log(boat[2]);
+// //  console.log(boat.length);
+// //  console.log(code[8]);
+// // ;
+// // console.log(boat[1]);
+
+
+ 
+  
+
---
+++ copy.js
@@ -0,0 +1,15 @@
+
+let items = [1,2,3,4,5,6,7,8,9,10];
+
+let sum = 0;
+
+for(let i=0;i<items.length;i++){
+
+    
+
+    sum = sum+items[i];
+    
+    console.log(sum);
+    i=i+1;
+
+}
